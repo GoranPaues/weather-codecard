3081f7b1dcde4d20fcaa8197b86f71ca93df2b34
Göran Paues <goran.paues@tradedoubler.com>
2020-08-14 23:29:27 +0200

    Improved output

--- get_weather/func.js
+++ get_weather/func.js
@@ -11,11 +11,21 @@ const getWeather = async url => {
   try {
     const response = await axios.get(url)
     let jsonData = response.data
+
+    let mainTempCelsius = Math.round(jsonData.main.temp - 273.15)
+    let tempMinCelsius = Math.round(jsonData.main.temp_min - 273.15)
+    let tempMaxCelsius = Math.round(jsonData.main.temp_max - 273.15)   
+    let sunriseTime = new Date(jsonData.sys.sunrise * 1e3).toISOString().slice(11, 16);
+    let sunsetTime = new Date(jsonData.sys.sunset * 1e3).toISOString().slice(11, 16);
+ 
     var codeCardJson = {
       template: 'template1',
-      title: jsonData.name + ' Weather',
-      subtitle: jsonData.main.temp + ' - ' +  jsonData.weather[0].description,
-      bodytext: 'More weather info...',
+      title: jsonData.name,
+      subtitle: `${mainTempCelsius}, min ${tempMinCelsius}, max ${tempMaxCelsius}`,
+      bodytext:  `${jsonData.weather[0].description[0].toUpperCase() +
+      jsonData.weather[0].description.substring(1)}
+Sun rises: ${sunriseTime}
+Sun sets:  ${sunsetTime}`,
       icon: jsonData.weather[0].icon,
       backgroundColor: 'white'
     }
